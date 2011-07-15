f6c1706b1831ef6e1337b34573fd9a21ed80cdee
ideasman42 <ideasman42@0b0f3fbf-c1da-6987-cff1-d5ee43f2aa54>
2011-07-15 04:44:14 +0000

    now builds and installs correctly

--- src/stubs/stubs.c
+++ src/stubs/stubs.c
@@ -2,3 +2,6 @@
 #include "BLI_memarena.h"
 
 void *MEM_callocN(size_t len, const char * str) { return (void *)0; };
+short MEM_freeN(void *vmemh) { return 0; }
+
+void *BLI_memarena_alloc(struct MemArena *ma, int size) { return (void *)0; }
