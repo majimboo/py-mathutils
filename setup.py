5631e501647f64877a883e8fc59d029f75c6bdee
ideasman42 <ideasman42@0b0f3fbf-c1da-6987-cff1-d5ee43f2aa54>
2011-07-17 07:30:15 +0000

    adding description

--- setup.py
+++ setup.py
@@ -1,5 +1,25 @@
 from distutils.core import setup, Extension
 
+desc = """\
+blender-mathutils module
+========================
+
+overview
+++++++++
+
+This module originated from blender (the opensource 3d package), where it has
+been used for some years in production as a utility module for use in areas
+including animation, games and mesh manipulation.
+
+This differs from 'numpy' in that it is computer graphics focused, combining
+Matrix and Vector types with rotation classes which is very useful for use with
+animation or anywhere Euler and Quaternion values are used frequently.
+
+This project is mainly a build system around the actively maintained mathutils
+code in blender to allow non blender related projects to make use of it. A link
+to the blender repository is used so the source never gets out of sync. 
+"""
+
 include_dirs = [
     "src/stubs",
     "src/blenlib",
